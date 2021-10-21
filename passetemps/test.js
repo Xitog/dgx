fa44fa7e1fa5deda475bdf39af9190974a1b4a20
Xitog <damien.gouteux@gmail.com>
2021-10-21 15:46:10 +0200

    New JSON favorites

---
+++ passetemps/test.js
@@ -0,0 +1,19 @@
+function getData(url, callback)
+{
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.onload = function()
+    {
+        let status = xhr.status;
+        if (status === 200)
+        {
+            callback(null, xhr.response);
+        }
+        else
+        {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
