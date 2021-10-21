54384fd55b1d91737bbce964bb6be83d485d262f
Xitog <damien.gouteux@gmail.com>
2021-10-21 16:29:45 +0200

    favorites json updated

--- passetemps/test.js
+++ passetemps/test.js
@@ -15,12 +15,12 @@ function callback(status, response)
 {
     console.log(status);
     console.log(response);
-    class_by_genre(response);
+    class_by(response, 'class');
 }
 
 function getProperty(code, type, property, data)
 {
-    if (data[type].hasOwnProperty(code) && data[type][code].hasOwnProperty(property))
+    if (data.hasOwnProperty(type) && data[type].hasOwnProperty(code) && data[type][code].hasOwnProperty(property))
     {
         return data[type][code][property];
     } else {
@@ -28,17 +28,17 @@ function getProperty(code, type, property, data)
     }
 }
 
-function class_by_genre(data)
+function class_by(data, criterion)
 {
     // Classify
     let genres = {};
     for (let song of data['titles'])
     {
-        if (!genres.hasOwnProperty(song['class']))
+        if (!genres.hasOwnProperty(song[criterion]))
         {
-            genres[song['class']] = [];
+            genres[song[criterion]] = [];
         }
-        genres[song['class']].push(song);
+        genres[song[criterion]].push(song);
     }
     // Display
     let tree = document.getElementById("tree");
@@ -46,8 +46,8 @@ function class_by_genre(data)
     for (let genre of Object.keys(genres))
     {
         let li = document.createElement("li");
-        console.log(getProperty(genre, "genres", "name", data));
-        li.innerText = getProperty(genre, "genres", "name", data);
+        console.log(getProperty(genre, criterion, "name", data));
+        li.innerText = getProperty(genre, criterion, "name", data);
         let ul = document.createElement("ul");
         li.appendChild(ul);
         for (let song of genres[genre])
