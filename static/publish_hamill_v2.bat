1479fa467f0c26cc4a308788b02a343709e0d4e5
Xitog <damien.gouteux@gmail.com>
2024-04-17 10:40:39 +0200

    Published with Hamill v2 Python

--- static/publish_hamill_v2.bat
+++ static/publish_hamill_v2.bat
@@ -1,3 +1,3 @@
-node ..\..\hamill\hamilljs\hamill.mjs -p .\hamill.config.json
+rem node ..\..\hamill\hamilljs\hamill.mjs -p .\hamill.config.json
 
-rem python ..\..\hamill\hamillpy\hamill\hamill.py -p .\hamill.config.json
+python ..\..\hamill\hamillpy\hamill\hamill.py -p .\hamill.config.json
